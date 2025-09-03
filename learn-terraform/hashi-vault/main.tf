22bc65968d5b664238f8e52e2046e5ca90e1293d
Nishchitha A <nischiashok0@gmail.com>
2025-09-03 16:28:35 +0530

    robo

--- learn-terraform/hashi-vault/main.tf
+++ learn-terraform/hashi-vault/main.tf
@@ -8,3 +8,13 @@ variable "token" {}
 data "vault_generic_secret" "secret" {
   path = "demo/ssh"
 }
+
+resource "local_file" "dev" {
+  content  = jsonencode(data.vault_generic_secret.secret.data)
+  filename = "/tmp/result"
+}
+
+resource "local_file" "test" {
+  content  = data.vault_generic_secret.secret.data["password"]
+  filename = "/tmp/result.pass"
+}
